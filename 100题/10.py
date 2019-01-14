7a2158f51e98e2c4a43a7a161b7f04c26cf3d1f1
gao <gxd1372843671@Gmail.com>
2019-01-14 11:58:29 +0800

    FirstDay

--- 100题/10.py
+++
@@ -1,7 +0,0 @@
-import time
-
-time.sleep( 5 )
-print(time.asctime(time.localtime(time.time())))
-
-
-
--- 100题/3.py
+++
@@ -1,7 +0,0 @@
-import math
-
-for s in range(-99,10000):
-    t=math.sqrt(s + 100)% 1
-    m=math.sqrt(s + 168+100)% 1
-    if t==0 and m==0:
-        print(s)
